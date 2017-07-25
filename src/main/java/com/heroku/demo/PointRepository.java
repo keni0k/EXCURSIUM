76c382aef43b2ed312f6293aa2348c47710ed3ff
Keni0k <dima-vers0@rambler.ru>
2017-07-25 23:14:32 +0800

    123

--- src/main/java/com/heroku/demo/PointRepository.java
+++ src/main/java/com/heroku/demo/PointRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
-    @Query("select p from point where p.id_of_guide = :id_of_guide")
+    @Query("select p from point p where p.id_of_guide = :id_of_guide")
     List<Record> findByGuide(@Param("id_of_guide") int id_of_guide);
 
 }
