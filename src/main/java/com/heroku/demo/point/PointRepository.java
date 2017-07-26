262c8b15ef0a2226f6c0977cff8fd7594737753d
Keni0k <dima-vers0@rambler.ru>
2017-07-26 15:15:47 +0800

    123

--- src/main/java/com/heroku/demo/point/PointRepository.java
+++ src/main/java/com/heroku/demo/point/PointRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import com.heroku.demo.point.Point;
 
 import java.util.List;
 
