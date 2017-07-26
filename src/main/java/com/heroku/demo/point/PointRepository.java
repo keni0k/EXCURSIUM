f95795b42285ba495c3f83ef9a776a0df2658d56
Keni0k <dima-vers0@rambler.ru>
2017-07-26 15:21:57 +0800

    123

--- src/main/java/com/heroku/demo/point/PointRepository.java
+++ src/main/java/com/heroku/demo/point/PointRepository.java
@@ -2,11 +2,7 @@ package com.heroku.demo.point;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import com.heroku.demo.point.Point;
-
-import java.util.List;
 
 @Repository
 public interface PointRepository extends JpaRepository<Point, Long> {
--- src/main/java/com/heroku/demo/record/RecordRepository.java
+++ src/main/java/com/heroku/demo/record/RecordRepository.java
@@ -1,11 +1,8 @@
 package com.heroku.demo.record;
 
 import com.heroku.demo.record.Record;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
