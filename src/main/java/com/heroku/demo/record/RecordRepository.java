6ae55c8bf123c4cbc8748cf944931648398d6c32
copy@hub2016 <andrewnikolaevich.2014@gmail.com>
2017-10-12 18:26:50 +0300

    123

--- src/main/java/com/heroku/demo/record/RecordRepository.java
+++ src/main/java/com/heroku/demo/record/RecordRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
 
+
 //    @Query("SELECT * FROM record WHERE record.what = :what")
 //    List<Record> findByType(@Param("what") int what);
 
