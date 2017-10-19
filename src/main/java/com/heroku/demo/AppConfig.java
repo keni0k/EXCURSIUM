8739fbd9e9de0d59e5f9d4db553dddab0cae343b
GERALD <dima-vers0@rambler.ru>
2017-10-19 19:13:06 +0800

    123

---
+++ src/main/java/com/heroku/demo/AppConfig.java
@@ -0,0 +1,53 @@
+package com.heroku.demo;
+
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter {
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        TomcatEmbeddedServletContainerFactory factory =
+                new TomcatEmbeddedServletContainerFactory();
+        return factory;
+    }
+    //start Thymeleaf specific configuration
+    @Bean(name ="templateResolver")
+    public ServletContextTemplateResolver getTemplateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("XHTML");
+        return templateResolver;
+    }
+    @Bean(name ="templateEngine")
+    public SpringTemplateEngine getTemplateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(getTemplateResolver());
+        return templateEngine;
+    }
+//    @Bean(name="viewResolver")
+//    public ThymeleafViewResolver getViewResolver(){
+//        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+//        viewResolver.setTemplateEngine(getTemplateEngine());
+//        return viewResolver;
+//    }
+    //end Thymeleaf specific configuration
+    @Bean(name ="messageSource")
+    public MessageSource getMessageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("/WEB-INF/i18/blogmsg");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+}
