506e71a10293f13ccfec6294d1d7032ad68831fa
Keni0k <dima-vers0@rambler.ru>
2017-11-03 17:30:39 +0900

    Языки

--- src/main/java/com/heroku/demo/AppConfig.java
+++ src/main/java/com/heroku/demo/AppConfig.java
@@ -10,9 +10,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-import org.springframework.web.servlet.mvc.support.ControllerClassNameHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
@@ -54,12 +52,6 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         return templateEngine;
     }
 
-    @Bean(name = "localeChangeInterceptor")
-    public LocaleChangeInterceptor localeChangeInterceptor() {
-        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
-        localeChangeInterceptor.setParamName("language");
-        return localeChangeInterceptor;
-    }
     @Bean(name = "locateResolver")
     public SessionLocaleResolver localeResolver(){
         SessionLocaleResolver localeResolver = new SessionLocaleResolver();
@@ -81,13 +73,6 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         messageSource.setUseCodeAsDefaultMessage(true);
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
-    }
-
-    @Bean
-    public ControllerClassNameHandlerMapping controllerClassNameHandlerMapping(){
-        ControllerClassNameHandlerMapping handlerMapping = new ControllerClassNameHandlerMapping();
-        handlerMapping.setInterceptors(localeChangeInterceptor());
-        return handlerMapping;
     }
 
     @Override
