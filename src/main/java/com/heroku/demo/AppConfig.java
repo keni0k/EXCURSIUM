631c4481bb8d1e32854d6fb82b5b1fe4e8f6cb46
Keni0k <dima-vers0@rambler.ru>
2017-11-03 17:44:15 +0900

    Языки

--- src/main/java/com/heroku/demo/AppConfig.java
+++ src/main/java/com/heroku/demo/AppConfig.java
@@ -8,8 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -51,6 +53,19 @@ public class AppConfig extends WebMvcConfigurerAdapter {
         templateEngine.setTemplateResolver(getTemplateResolver());
         return templateEngine;
     }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
 
     @Bean(name = "locateResolver")
     public SessionLocaleResolver localeResolver(){
