972b905ea84429fdc0c85320c6b68cdc85102b00
Keni0k <dima-vers0@rambler.ru>
2017-11-04 20:59:05 +0900

    Валидность

---
+++ src/main/java/com/heroku/demo/event/EventController.java
@@ -0,0 +1,9 @@
+package com.heroku.demo.event;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/events/yes")
+public class EventController {
+}
