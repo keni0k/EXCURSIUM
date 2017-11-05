58ba984e0b89d2097e56bf518af0878379e40703
Keni0k <dima-vers0@rambler.ru>
2017-11-06 03:32:32 +0900

    SECURITY

--- src/main/java/com/heroku/demo/DatabaseConfig.java
+++ src/main/java/com/heroku/demo/DatabaseConfig.java
@@ -5,16 +5,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 public class DatabaseConfig {
     @Bean (name = "dataSource")
     @Primary
-    @ConfigurationProperties("classpath:application.properties")
+    @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource dataSource() {
         return DataSourceBuilder.create().build();
     }
