2b7a2c91d77efd1650346d7345986d6bc0f1d189
Keni0k <dima-vers0@rambler.ru>
2017-11-05 22:46:21 +0900

    SECURITY

--- src/main/java/com/heroku/demo/DatabaseConfig.java
+++ src/main/java/com/heroku/demo/DatabaseConfig.java
@@ -25,10 +25,12 @@ public class DatabaseConfig {
     public DataSource dataSource() {
         org.apache.tomcat.jdbc.pool.DataSource ds = new org.apache.tomcat.jdbc.pool.DataSource();
 
-        String url = "postgres://qmnjagkdtvydwo:8d065c8a74f33586eb8af68ff8263f7c1b4e8a10213cb1ec22e8037cd27c4168@ec2-54-225-113-161.compute-1.amazonaws.com:5432/dc8bu9fgr1ksnh";
+        String url = "jdbc:postgresql://qmnjagkdtvydwo:8d065c8a74f33586eb8af68ff8263f7c1b4e8a10213cb1ec22e8037cd27c4168@ec2-54-225-113-161.compute-1.amazonaws.com:5432/dc8bu9fgr1ksnh";
 
         ds.setDriverClassName("org.postgresql.Driver");
         ds.setUrl(url);
+        ds.setUsername("qmnjagkdtvydwo");
+        ds.setPassword("8d065c8a74f33586eb8af68ff8263f7c1b4e8a10213cb1ec22e8037cd27c4168");
 
         return ds;
     }
