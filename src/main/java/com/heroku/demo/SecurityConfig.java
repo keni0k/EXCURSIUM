8705457e52bef9680782210742bb5edf9e5fb574
Keni0k <dima-vers0@rambler.ru>
2017-11-06 03:14:42 +0900

    SECURITY

--- src/main/java/com/heroku/demo/SecurityConfig.java
+++ src/main/java/com/heroku/demo/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.heroku.demo;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,7 +14,7 @@ import javax.sql.DataSource;
 @EnableAutoConfiguration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Qualifier("dataSource")
+
     @Autowired
     DataSource dataSource;
 
