5d908840c173fa1d1ed5aba90f86711967c144ab
Keni0k <dima-vers0@rambler.ru>
2017-11-15 12:35:46 +0900

    Optimization

--- src/main/java/com/heroku/demo/event/EventRepository.java
+++ src/main/java/com/heroku/demo/event/EventRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT c FROM Event AS c WHERE c.price<=:price_up AND c.price>=:price_down " +
             "AND (c.language=:language OR :language=-1) AND (c.category=:category OR :category=-1) " +
-            "AND (c.type=0 OR is_all)")
+            "AND (c.type=0 OR :is_all)")
     List<Event> getByFilter(@Param("price_up") Integer priceUp, @Param("price_down") Integer priceDown,
                             @Param("category") Integer category, @Param("language") Integer language,
                             @Param("is_all") Boolean isAll);
