1cc8e63ff848a96057595e368da497588990ba1c
Keni0k <dima-vers0@rambler.ru>
2017-11-25 15:08:37 +0900

    Security

--- src/main/java/com/heroku/demo/utils/TokenService.java
+++ src/main/java/com/heroku/demo/utils/TokenService.java
@@ -1,7 +1,10 @@
 package com.heroku.demo.utils;
 
+import com.heroku.demo.person.PersonController;
 import com.heroku.demo.token.TokenCookies;
 import com.heroku.demo.token.TokenRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.web.authentication.rememberme.PersistentRememberMeToken;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
@@ -14,6 +17,8 @@ public class TokenService implements PersistentTokenRepository {
 
     private TokenRepository repository;
 
+    private static final Logger logger = LoggerFactory.getLogger(PersonController.class);
+
     @Autowired
     public TokenService(TokenRepository tokenRepository){
         repository = tokenRepository;
@@ -21,6 +26,9 @@ public class TokenService implements PersistentTokenRepository {
 
     @Override
     public void createNewToken(PersistentRememberMeToken token) {
+
+        logger.info("CREATE_TOKEN: username= " + token.getUsername() + " series= " + token.getSeries() + " tokenValue= " + token.getTokenValue());
+
         repository.save(new TokenCookies(token.getUsername(), token.getSeries(),
                 token.getTokenValue(), token.getDate()));
     }
@@ -31,6 +39,7 @@ public class TokenService implements PersistentTokenRepository {
         token.setTokenValue(value);
         token.setSeries(series);
         token.setDate(lastUsed);
+        logger.info("UPDATE_TOKEN: username= " + token.getUsername() + " series= " + token.getSeries() + " tokenValue= " + token.getTokenValue());
         repository.save(token);
     }
 
@@ -43,6 +52,7 @@ public class TokenService implements PersistentTokenRepository {
     public void removeUserTokens(String username) {
         TokenCookies token = repository.findByUsername(username);
         if (token != null) {
+            logger.info("REMOVE_TOKEN: username= " + token.getUsername() + " series= " + token.getSeries() + " tokenValue= " + token.getTokenValue());
             repository.delete(token);
         }
     }
