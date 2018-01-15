eaa31835bdd1d81be52dc3b9aaab281ec27ffc3b
Keni0k <dima-vers0@rambler.ru>
2018-01-15 19:04:36 +0800

    Отзыв на скриншоте заказа, имя, все дела.

--- src/main/java/com/heroku/demo/order/OrderController.java
+++ src/main/java/com/heroku/demo/order/OrderController.java
@@ -58,7 +58,7 @@ public class OrderController {
                     return "order/order";
                 }
             }
-        }
+        } else return "redirect:/login";
         return "error/403";
     }
 
