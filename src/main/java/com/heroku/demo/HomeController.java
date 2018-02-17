fa95da76628aa421aa43c8a1fc7aa756eb27107a
Keni0k <dima-vers0@rambler.ru>
2018-02-17 16:58:31 +0800

    Tourist && Guide

--- src/main/java/com/heroku/demo/HomeController.java
+++ src/main/java/com/heroku/demo/HomeController.java
@@ -124,7 +124,7 @@ public class HomeController {
 
                 putImg(serverFile.getAbsolutePath(), photoToken);
                 photoRepository.save(photo);
-                message.append("<img src=\"").append(Consts.URL_PATH).append(photoToken).append("\" class=\"img-rounded upload-img mb-1\"/>");
+                message.append("<img src=\"").append(Consts.URL_PATH).append(photoToken).append("\" class=\"img-rounded upload-img\"/>");
             } catch (Exception e) {
                 return "You failed to upload " + name + " => " + e.getMessage();
             }
